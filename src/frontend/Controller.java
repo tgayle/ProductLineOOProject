da938ce68f017b668bca4ad48e6d616186de5be1
tgayle <travisgayledev@gmail.com>
2019-09-17 12:36:27 -0400

    Week 3: Add buttons to each tab

--- src/frontend/Controller.java
+++ src/frontend/Controller.java
@@ -1,4 +1,16 @@
 package frontend;
 
 public class Controller {
+
+  public void prodLineBtnClicked() {
+    System.out.println("Hello from the production line!");
+  }
+
+  public void recordBtnClicked() {
+    System.out.println("Hello from record production!");
+  }
+
+  public void prodLabBtnClicked() {
+    System.out.println("Hello from the production lab!");
+  }
 }
